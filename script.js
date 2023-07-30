2c8843530c22cb3e30dce9fda7b084298bbf4ac4
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-07-30 22:03:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+var para = document.getElementById("status");
+var button =  document.getElementById("enterBtn");
+
+button.addEventListener('click', ()=>{
+	
+	let h1 = document.createElement("h1");
+	h1.textContent = "Entered Metaverse";
+	para.parentNode.replaceChild(h1,para);
+ 
+	
+});
